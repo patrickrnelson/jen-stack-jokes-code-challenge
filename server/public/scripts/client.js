036eff9325e1acb1920aaa5cb944e3b649bc4eb2
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-08 13:29:17 -0600

    app.get is ready in the server, and ajax GET is set up in the client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,20 @@
 console.log('client.js sourced');
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 function onReady() {
-    console.log('DOM ready');
+  console.log('DOM ready');
 }
+
+function getJokes() {
+  $.ajax({
+    type: 'GET',
+    url: '/listOfJokes'
+  }).then(function, (response) {
+    console.log('Succesful GET of Jokes');
+    //append the incoming array to the DOM
+    appendListOfJokes(response);
+  })
+};
+
+appendListOfJokes
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
-const bodyParser = require( 'body-parser' );
+const bodyParser = require('body-parser');
 const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
@@ -8,31 +8,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 let jokes = [
   {
-    whoseJoke: "Danny",
-    jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat!"
+    whoseJoke: 'Danny',
+    jokeQuestion: 'Why do scuba divers fall backwards out of boats?',
+    punchLine: 'If they fell forwards they’d still be in the boat!',
   },
   {
-    whoseJoke: "Luke",
-    jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
+    whoseJoke: 'Luke',
+    jokeQuestion: 'Two fish are in a tank. What did one fish say to the other?',
+    punchLine: 'Do you know how to drive this thing?',
   },
   {
-    whoseJoke: "Millie",
-    jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
+    whoseJoke: 'Millie',
+    jokeQuestion: 'What do you call a pile of cats?',
+    punchLine: 'A meowntain!',
   },
   {
-    whoseJoke: "dEv",
-    jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs!"
+    whoseJoke: 'dEv',
+    jokeQuestion: 'Why should you not play cards in the forest?',
+    punchLine: 'Too many Cheetahs!',
   },
   {
-    whoseJoke: "Scott",
-    jokeQuestion: "I went to the zoo the other day, it had one dog...",
-    punchLine: "It was a shih tzu."
-  }
+    whoseJoke: 'Scott',
+    jokeQuestion: 'I went to the zoo the other day, it had one dog...',
+    punchLine: 'It was a shih tzu.',
+  },
 ];
+
+// GET to send the jokes array to the server
+app.get('/listOfJokes', (req, res) => {
+  console.log('Successful GET');
+  res.send(jokes);
+});
 
 // serve back static files
 app.use(express.static('server/public'));
